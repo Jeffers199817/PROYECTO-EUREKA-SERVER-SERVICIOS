d39b246b0fccf8a9a5d75d578dd6056548823557
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-01-23 16:47:11 -0500

    Integración de la logica de la api de post-service en el controller

--- posts-service/src/main/java/com/milenyumsoft/posts_service/controller/PostsController.java
+++ posts-service/src/main/java/com/milenyumsoft/posts_service/controller/PostsController.java
@@ -1,7 +1,32 @@
 package com.milenyumsoft.posts_service.controller;
 
+import com.milenyumsoft.posts_service.model.Post;
+import com.milenyumsoft.posts_service.service.IPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/posts")
 public class PostsController {
+
+    @Autowired
+    private IPostService postService;
+
+    @Value("${server.port}")
+    private int serverPort;
+
+
+    @GetMapping("{/user_id}")
+    public List<Post> getPostsByUserId(@PathVariable Long user_id) {
+        System.out.println("================Estoy en el puerto " + serverPort);
+    return postService.getPostsByUser(user_id);
+    }
+
+
 }
