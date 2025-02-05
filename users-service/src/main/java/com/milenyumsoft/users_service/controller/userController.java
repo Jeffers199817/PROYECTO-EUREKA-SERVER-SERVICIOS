35ce676445ceef97df0f7a3b34b89eb0a0936fff
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-02-04 23:16:29 -0500

    integración d la carpeta controller

--- users-service/src/main/java/com/milenyumsoft/users_service/controller/userController.java
+++ users-service/src/main/java/com/milenyumsoft/users_service/controller/userController.java
@@ -1,13 +1,28 @@
 package com.milenyumsoft.users_service.controller;
 
+import com.milenyumsoft.users_service.dto.UserDTO;
+import com.milenyumsoft.users_service.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class userController {
+
+
+    @Autowired
+    private IUserService servUsu;
+
+
+    @GetMapping("/posts/{user_id")
+    public UserDTO getUserAndPosts (@PathVariable Long user_id){
+
+        UserDTO user = servUsu.getUserAndPosts(user_id);
+        return user;
+    }
 
 
 
